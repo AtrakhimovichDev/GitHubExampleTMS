40f27331d2dc83e85fd715124f0f01838f9a62dd
Andrei Atrakhimovich <atrakhimovich95@gmail.com>
2025-02-26 19:41:00 +0300

    Initial Commit

---
+++ Lesson3Github/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson3Github
+//
+//  Created by Andrei Atrakhimovich on 26.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
